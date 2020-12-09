2ae29b10e6ac31aa6457d01739fc3b40bff1278a
Steve Min <stevekmin@gmail.com>
2020-12-09 09:40:12 -0800

    setup location marker component

--- wildfire-tracker/src/components/LocationMarker.js
+++ wildfire-tracker/src/components/LocationMarker.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/fire-alert';
 
-const LocationMarker = () => {
-  return <div></div>;
+const LocationMarker = ({ lat, lng, onClick }) => {
+  return (
+    <div className='location-marker' onClick={onClick}>
+      <Icon icon={location} className='location-icon'></Icon>
+    </div>
+  );
 };
 
 export default LocationMarker;
