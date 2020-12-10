711c562a69707c0bdc295b10bfb1966f2de26f78
Steve Min <stevekmin@gmail.com>
2020-12-10 07:25:20 -0800

    set state back to defualt add dependency array after rfunction call

--- wildfire-tracker/src/App.js
+++ wildfire-tracker/src/App.js
@@ -11,8 +11,14 @@ function App() {
       setLoading(true);
       const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
       const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
     };
-  });
+
+    fetchEvents();
+    console.log(events);
+  }, []);
 
   return (
     <div>
