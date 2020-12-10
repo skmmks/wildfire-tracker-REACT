1f42fcf9aa0c7cf95520573e703a304433e6b26d
Steve Min <stevekmin@gmail.com>
2020-12-10 07:17:33 -0800

    import and setup useState var

--- wildfire-tracker/src/App.js
+++ wildfire-tracker/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Map from './components/Map';
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   return (
     <div>
       <h1>Welcome to the App</h1>
