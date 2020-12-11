d919d56c9333f3aa1ff21dc7c17d7b32c6d8f45a
Steve Min <stevekmin@gmail.com>
2020-12-11 07:28:23 -0800

    import loader component and add to app component

--- wildfire-tracker/src/App.js
+++ wildfire-tracker/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Map from './components/Map';
+import Loader from './components/Loader';
 
 function App() {
   const [eventData, setEventData] = useState([]);
@@ -19,7 +20,7 @@ function App() {
     fetchEvents();
   }, []);
 
-  return <div>{!loading ? <Map eventData={eventData} /> : <h1>Loading</h1>}</div>;
+  return <div>{!loading ? <Map eventData={eventData} /> : <Loader />}</div>;
 }
 
 export default App;
