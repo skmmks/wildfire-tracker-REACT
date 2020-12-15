9d906f4191d66ea6b34235dacfc789660fc6f2d3
Steve Min <stevekmin@gmail.com>
2020-12-15 07:42:24 -0800

    add info id and title to locationtinfobox component

--- wildfire-tracker/src/components/LocationInfoBox.js
+++ wildfire-tracker/src/components/LocationInfoBox.js
@@ -4,6 +4,14 @@ const LocationInfoBox = ({ info }) => {
   return (
     <div className='location-info'>
       <h2>Event Location Info</h2>
+      <ul>
+        <li>
+          ID: <strong>{info.id}</strong>
+        </li>
+        <li>
+          TITLE: <strong>{info.title}</strong>
+        </li>
+      </ul>
     </div>
   );
 };
