12649a82becbba3167b1959695570edf7c281852
Steve Min <stevekmin@gmail.com>
2020-12-15 07:32:44 -0800

    add locationinfobox component setup initial by passing info prop

---
+++ wildfire-tracker/src/components/LocationInfoBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LocationInfoBox = ({ info }) => {
+  return (
+    <div className='location-info'>
+      <h2>Event Location Info</h2>
+    </div>
+  );
+};
+
+export default LocationInfoBox;
