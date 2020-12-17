79e3e999a9da6f1dd9edf3c4f441ac99a2b8af62
Steve Min <stevekmin@gmail.com>
2020-12-17 07:16:24 -0800

    import icons into header component add Icon react component into header component

--- wildfire-tracker/src/components/Header.js
+++ wildfire-tracker/src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import locationIcon from '@iconify/icons-mdi/fire-alert';
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <header className='header'>
+      <h1>
+        <Icon icon={locationIcon} />
+      </h1>
+    </header>
+  );
 };
 
 export default Header;
